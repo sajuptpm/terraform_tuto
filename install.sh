d2d502a895c4f7a6ac4821fc68396b41134e2535
root <you@example.com>
2020-12-16 09:22:00 +0000

    updated

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
+
+git config --global user.username "sajuptpm"
+git config --global user.email "you@example.com"
+
+apt update
+apt install wget -y
+apt install unzip -y
+
 wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
 unzip ./terraform_0.12.26_linux_amd64.zip
-sudo mv terraform /usr/local/bin
+mv terraform /usr/local/bin
 terraform version
 rm -rf ./terraform_0.12.26_linux_amd64.zip
